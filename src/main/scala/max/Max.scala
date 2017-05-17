fe251842ea3ac5b840ae6f260aedfcf8c8963a9d
Fabián Novoa <fabiansteven90@gmail.com>
2017-05-17 16:17:45 -0500

    Fixing max method more short

--- src/main/scala/max/Max.scala
+++ src/main/scala/max/Max.scala
@@ -7,19 +7,13 @@ object Max {
 
 
   def max(ints: List[Int]): Int = {
+
+    def maxAccum(ints: List[Int], theMax: Int): Int = {
+      if (ints.isEmpty) theMax
+      else maxAccum(ints.tail, if (ints.head < theMax) theMax else ints.head)
+    }
     maxAccum(ints, 0)
   }
 
-  def maxAccum(ints: List[Int], theMax: Int): Int = {
-    if (ints.isEmpty) theMax
-    else if (ints.head > theMax) {
-      if (ints.length == 1) ints.head
-      else maxAccum(ints.tail, ints.head)
-    }
-    else {
-      if (ints.length == 1) theMax
-      else maxAccum(ints.tail, theMax)
-    }
-  }
 
 }
